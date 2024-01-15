4fd5cce6332c7e5faadc4b373b9abdf814e2fb1c
Ilya Nevolin <ilja.nevolin@gmail.com>
2024-01-15 09:59:51 +0000

    fix

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # DiscordEarsGo
 
-This works only on a Linux x86 platform
+A speech-to-text bot for Discord written in GoLang. Can be useful for hearing impaired and deaf people. Try the bot for yourself on our Discord server: https://discord.gg/ApdTMG9
 
-## Installation
-- clone this repository and cd inside
-- create `.env` file to provide your Discord bot token: `TOKEN=...`
-- run `VOSK_PATH=$(pwd)/vosk-linux-x86_64-0.3.45 LD_LIBRARY_PATH=$VOSK_PATH CGO_CPPFLAGS="-I $VOSK_PATH" CGO_LDFLAGS="-L $VOSK_PATH" go run ears.go`
+Note: This works only on a Linux x86 platform
+
+
+## Setup
+
+In your Discord Developers Bot settings, you need to enable these intents:
+
+![image](https://github.com/inevolin/DiscordEarsBot/assets/53948000/6e926a75-a709-435a-b4f8-e9f8f0226856)
+
+1. You may need to install opus `apt-get install opus-tools`
+2. Clone this repository and cd inside
+3. Create `.env` file to provide your Discord bot token: `TOKEN=...`
+
+## Usage
+- Run `VOSK_PATH=$(pwd)/vosk-linux-x86_64-0.3.45 LD_LIBRARY_PATH=$VOSK_PATH CGO_CPPFLAGS="-I $VOSK_PATH" CGO_LDFLAGS="-L $VOSK_PATH" go run ears.go`
+- Or use `go build` to (with the above CLI prefix) and run the binary
+
+## Usage
+1. Enter one of your voice channels.
+2. In one of your text channels type: `*join`, the bot will join the voice channel.
+3. Everything said within that channel will be transcribed into text (as long as the bot is within the voice channel).
+4. Type `*leave` to make the bot leave the voice channel.
--- ears.go
+++ ears.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -16,12 +17,16 @@ import (
 	"layeh.com/gopus"
 )
 
-var model, _ = vosk.NewModel("./vosk_models/en")
-var stt, _ = vosk.NewRecognizer(model, 48000)
-var speakers, _ = gopus.NewDecoder(48000, 1)
+type STTResult struct {
+	Text string `json:"text"`
+}
 
-func handleVoice(c chan *discordgo.Packet) {
-	log.Println("handleVoice")
+var model, _ = vosk.NewModel("./vosk_models/en")
+var stt, _ = vosk.NewRecognizer(model, 48000) // 48kHz
+var speakers, _ = gopus.NewDecoder(48000, 1)  // 48kHz and mono-channel
+var voiceConnection *discordgo.VoiceConnection
+
+func handleVoice(client *discordgo.Session, channelID string, user string, c chan *discordgo.Packet) {
 	var buffer = new(bytes.Buffer)
 	for {
 		select {
@@ -32,19 +37,24 @@ func handleVoice(c chan *discordgo.Packet) {
 			if buffer == nil {
 				buffer = new(bytes.Buffer)
 			}
-			packet, _ := speakers.Decode(s.Opus, 960, false)
+			packet, _ := speakers.Decode(s.Opus, 960, false) // frameSize is 960 (20ms)
 			pcm := new(bytes.Buffer)
 			binary.Write(pcm, binary.LittleEndian, packet)
 			buffer.Write(pcm.Bytes())
 			stt.AcceptWaveform(pcm.Bytes())
 
-			var dur float32 = float32(len(buffer.Bytes())) / 48000 / 2
-			// silence packet
+			var dur float32 = (float32(len(buffer.Bytes())) / 48000 / 2) // duration of audio
+
+			// When silence packet detected, send result (skip audio shorter than 500ms)
 			if dur > 0.5 && len(s.Opus) == 3 && s.Opus[0] == 248 && s.Opus[1] == 255 && s.Opus[2] == 254 {
 				log.Println("dur", dur)
-				log.Println(stt.FinalResult()) // TODO: parse json
+				var result STTResult
+				json.Unmarshal([]byte(stt.FinalResult()), &result)
+				if len(result.Text) > 0 {
+					log.Println(fmt.Sprintf("%s: %s", user, result.Text))
+					client.ChannelMessageSend(channelID, fmt.Sprintf("%s: %s", user, result.Text))
+				}
 				buffer.Reset()
-
 			}
 		}
 	}
@@ -52,79 +62,59 @@ func handleVoice(c chan *discordgo.Packet) {
 
 func handleMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// check if the message is "!airhorn"
 	if strings.HasPrefix(m.Content, "*join") {
-
-		// Find the channel that the message came from.
-		c, err := s.State.Channel(m.ChannelID)
-		if err != nil {
-			// Could not find channel.
-			return
-		}
-
 		// Find the guild for that channel.
-		g, err := s.State.Guild(c.GuildID)
+		guild, err := s.State.Guild(m.GuildID)
 		if err != nil {
-			// Could not find guild.
+			fmt.Println("Could not find guild", err)
 			return
 		}
 
 		// Look for the message sender in that guild's current voice states.
-		for _, vs := range g.VoiceStates {
+		for _, vs := range guild.VoiceStates {
 			if vs.UserID == m.Author.ID {
-				// join channel
-				log.Println(g.ID, vs.ChannelID)
-				v, err := s.ChannelVoiceJoin(g.ID, vs.ChannelID, true, false)
+				// join channel where user is at
+				v, err := s.ChannelVoiceJoin(guild.ID, vs.ChannelID, true, false)
 				if err != nil {
 					fmt.Println("failed to join voice channel:", err)
 					return
 				}
-
-				// TODO: when leave command is given, close objects:
-				// go func() {
-				// 	time.Sleep(10 * time.Second)
-				// 	close(v.OpusRecv)
-				// 	v.Close()
-				// }()
-
-				go handleVoice(v.OpusRecv)
+				voiceConnection = v
+				handleVoice(s, m.ChannelID, m.Author.Username, v.OpusRecv)
 			}
 		}
 
+	} else if strings.HasPrefix(m.Content, "*leave") {
+		voiceConnection.Disconnect()
+		close(voiceConnection.OpusRecv)
+		voiceConnection.Close()
 	}
 }
 
-func ready(s *discordgo.Session, event *discordgo.Ready) {
-
-	// Set the playing status.
-	s.UpdateGameStatus(0, "!airhorn")
-}
-
 func main() {
+	// get TOKEN from .env
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
 	Token := os.Getenv("TOKEN")
 
+	// init discordgo client
 	client, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session:", err)
 		return
 	}
-	client.AddHandler(ready)
 	defer client.Close()
 
-	// We only really care about receiving voice and message updates.
+	// Intents for receiving voice, message updates, read members and send new messages
 	client.Identify.Intents = discordgo.IntentsAll
 	client.AddHandler(handleMessage)
 	client.StateEnabled = true
-
 	err = client.Open()
 	if err != nil {
 		fmt.Println("error opening connection:", err)
